ce6944f45b6f2214c141032659a144d5c5d27635
Serious014i <82103897+Serious014i@users.noreply.github.com>
2022-06-25 19:44:02 +0400

    Add files via upload

---
+++ back/src/main/java/com/wetravel/demo/WetravelApplication.java
@@ -0,0 +1,13 @@
+package com.wetravel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetravelApplication.class, args);
+	}
+
+}
